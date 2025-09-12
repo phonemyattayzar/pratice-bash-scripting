75c6ad8e847add77f6290804d4a8b6c95fa31383
phonemyattayzarkyaw <pmtzk2000@gmail.com>
2025-09-12 15:00:55 +0630

    Add how to get directory path in base-name.sh

--- base-name.sh
+++ base-name.sh
@@ -10,3 +10,14 @@ FILE=$(basename $FULLPATHTOFILE)
 # Or Another Way
 
 NEWFILE=${FULLPATHTOFILE##*/}
+
+
+# You have a full file path, for example
+MYPATHTOFILE="/home/user/docs/report.txt"
+
+# You want just the directory part (/home/user/docs) without the filename.
+DIR=$(dirname $MYPATHTOFILE)
+
+# Or
+
+DIR=${MYPATHTOFILE%/*}
