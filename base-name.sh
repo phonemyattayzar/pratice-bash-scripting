89e6c11ed11dd7390648433bdc1a0f21e0dd55f1
phonemyattayzarkyaw <pmtzk2000@gmail.com>
2025-09-12 15:09:00 +0630

    how to declare array in bash

--- base-name.sh
+++ base-name.sh
@@ -2,22 +2,23 @@
 
 # You have a full file path, for example
 FULLPATHTOFILE="/home/user/docs/report.txt"
-
 # You want just the filename (report.txt) without the directories.
 FILE=$(basename $FULLPATHTOFILE)
-
-
 # Or Another Way
-
 NEWFILE=${FULLPATHTOFILE##*/}
 
 
 # You have a full file path, for example
 MYPATHTOFILE="/home/user/docs/report.txt"
-
 # You want just the directory part (/home/user/docs) without the filename.
 DIR=$(dirname $MYPATHTOFILE)
-
 # Or
-
 DIR=${MYPATHTOFILE%/*}
+
+# Declare Array
+SCORE=(first second third home)
+
+echo ${SCORE[0]}   # first element → first
+echo ${SCORE[2]}   # third element → third
+
+
